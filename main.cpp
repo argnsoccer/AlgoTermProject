13e44181fefe8c182fa40beee9476794e17ee47f
Andre Gras <amgras@smu.edu>
2018-03-06 20:31:59 -0600

    Graph generates with running time and avg degree and total edges....now to plot

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #define _USE_MATH_DEFINES
 
 #include <iostream>
-
+#include <stdio.h>
+#include <time.h>
 #include <stdlib.h>
 #include <vector>
 #include <math.h>
@@ -12,6 +13,8 @@ struct Node{
     double x;
     double y;
     vector <Node*> edges;
+    int degree;
+
 };
 
 
@@ -34,10 +37,14 @@ int main()
 
     cout << "r = " << r << endl;
     
+    time_t startTimer;
+    time(&startTimer);
+
     vector <Node*> verts;
     for (int i = 0; i < numVerts; i++)//create random vertices
     {
         Node* v = new Node();
+        v->degree = 0;
         v->x = ((double) rand() / (RAND_MAX));
         v->y = ((double) rand() / (RAND_MAX));
         verts.push_back(v);
@@ -48,9 +55,11 @@ int main()
     for (int j = 0; j < verts.size(); j++)
     {
         Node * cur = verts.at(j);
-        Node * nextNode = verts.at(j+1);
-        while(j+1 < verts.size())//bounds check on verts in list
+        int nextI = j+1;
+
+        while(nextI < verts.size())//bounds check on verts in list
         {
+            Node * nextNode = verts.at(nextI);
             changeX = cur->x - nextNode->x;//distance in x
             changeY = cur->y - nextNode->y;//distance in y
             changeX *= changeX;//squared
@@ -59,10 +68,44 @@ int main()
             if(distance <= r)
             {
                 cur->edges.push_back(nextNode);
+                cur->degree++;
                 nextNode->edges.push_back(cur);
+                nextNode->degree++;
             }
+            nextI++;
         }
     }
+    time_t endTimer;
+    time(&endTimer);
+
+    double seconds = endTimer - startTimer;
+    cout << "Time taken to generate vertices and edges: " << seconds << " seconds" << endl;
+
+    int total = verts.at(0)->degree;
+    int min = verts.at(0)->degree;
+    int max = verts.at(0)->degree;
+    int totalEdges = verts.at(0)->edges.size();
+    for (int a = 1; a < verts.size(); a++)
+    {
+        total += verts.at(a)->degree;
+        totalEdges += verts.at(a)->edges.size();
+        if(verts.at(a)->degree < min)
+        {
+            min = verts.at(a)->degree;
+        }
+        if(verts.at(a)->degree > max)
+        {
+            max = verts.at(a)->degree;
+        } 
+
+    }
+    cout << "Max degree: " << max << endl;
+    cout << "Min degree: " << min << endl;
+    double density = total/numVerts;
+    cout << "Average degree: " << density << endl;
+    cout << "Total edges: " << totalEdges << endl;
+
+
 
 }
 
